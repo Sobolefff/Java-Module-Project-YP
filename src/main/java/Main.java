49f42feb01afbd33160790599f2e514aebf1a7b1
Soboleff <petr.soboleff@gmail.com>
2025-09-19 13:54:28 +0300

    dev вынес классы в отдельные файлы и дописал логику в Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,59 +1,39 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+
         for (int i = 1; i <= 3; i++) {
             String inputName;
+            int inputSpeed;
+
             while (true) {
                 System.out.print("— Введите название машины №" + i + ":");
-                inputName = scanner.nextLine().trim();
-                /*System.out.print("— Введите скорость машины №" + i + ":");
-                int inputSpeed = scanner.nextInt();*/
+                inputName = scanner.next().trim();
                 if (!inputName.isEmpty()) break;
-                else {
-                    System.out.println("Название автомобиля не должно быть пустым");
-                    continue;
+                System.out.println("Название автомобиля не должно быть пустым");
+            }
+            while (true) {
+                System.out.print("— Введите скорость машины №" + i + ":");
+                try {
+                    inputSpeed = scanner.nextInt();
+                    if (inputSpeed > 0 && inputSpeed <= 250) break;
+                    else System.out.println("Скорость должна быть от 1 до 250");
+                }
+                catch (InputMismatchException e) {
+                    System.out.println("Введите целое число");
+                    scanner.next();
                 }
             }
+            Car car = new Car(inputName, inputSpeed);
+            race.updateLeader(car);
         }
 
-    }
-}
+        System.out.println("Самая быстрая машина: " + race.getLeaderName());
 
-class Car {
-
-    String name;
-    int speed;
-
-    Car(String name, int speed) {
-        this.name = name;
-        this.speed = speed;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public int getSpeed() {
-        return speed;
-    }
-
-}
-
-class Race {
-    String leaderName = "";
-    int leaderDistance = 0;
-    public static final int RACE_HOURS = 24;
-    void getLeader(Car car) {
-        int distance  = RACE_HOURS * car.getSpeed();
-        if (distance > leaderDistance) {
-            leaderDistance = distance;
-            leaderName = car.getName();
-        }
-    }
-    public String getLeaderName() {
-        return leaderName;
     }
 }
