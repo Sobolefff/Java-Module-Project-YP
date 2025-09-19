090d31de2e444c7aab36e0e34174fc9b09e26869
Mr_Soboleff <petr.soboleff@gmail.com>
2025-09-19 12:12:11 +0300

    dev обьявил классы Car, Race. Сделал цикл для считывания с клавиатуры названия 3 машин

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,59 @@
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i <= 3; i++) {
+            String inputName;
+            while (true) {
+                System.out.print("— Введите название машины №" + i + ":");
+                inputName = scanner.nextLine().trim();
+                /*System.out.print("— Введите скорость машины №" + i + ":");
+                int inputSpeed = scanner.nextInt();*/
+                if (!inputName.isEmpty()) break;
+                else {
+                    System.out.println("Название автомобиля не должно быть пустым");
+                    continue;
+                }
+            }
+        }
+
+    }
+}
+
+class Car {
+
+    String name;
+    int speed;
+
+    Car(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+
+}
+
+class Race {
+    String leaderName = "";
+    int leaderDistance = 0;
+    public static final int RACE_HOURS = 24;
+    void getLeader(Car car) {
+        int distance  = RACE_HOURS * car.getSpeed();
+        if (distance > leaderDistance) {
+            leaderDistance = distance;
+            leaderName = car.getName();
+        }
+    }
+    public String getLeaderName() {
+        return leaderName;
     }
 }
